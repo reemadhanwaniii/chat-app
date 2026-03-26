327bae0f9d5f67f5ffe094395e57869ca60e5605
Reema Dhanwani <reemadhanwani1234@gmail.com>
2026-03-26 22:32:43 +0530

    Added client server communication

--- index.js
+++ index.js
@@ -5,6 +5,27 @@ const server = http.createServer(app);
 const socketio = require('socket.io');
 const io = socketio(server);
 
+app.use('/',express.static(__dirname+'/public'));
+
+/**
+ * -> Triggered when a client connects via Socket.IO
+   -> Each client gets a unique socket object
+ */
+io.on('connection',(socket) => {
+    console.log("A user connected",socket.id);
+
+    // Listens for a custom event from Client
+    socket.on('from_client',() => {
+        console.log("Event coming from client");
+    })
+
+    // Every 2 seconds → sends event to that specific client
+   //  This is server push (real-time)
+    setInterval(()=>{
+        socket.emit("from_server");
+    },2000);
+
+});
 
 
 server.listen(3000,()=>{
